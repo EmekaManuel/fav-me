733cc3714ee08d3b559d41d4ccb043a53cb712ea
Onyewuchi Emeka <marrnuel123@.com>
2024-10-13 15:25:11 +0100

    fix(): fix deserialization error thrown when building

--- programs/fav-me/src/lib.rs
+++ programs/fav-me/src/lib.rs
@@ -1,12 +1,15 @@
 use anchor_lang::prelude::*;
-use anchor_lang::system_program::System;
+use anchor_lang::system_program::System; // remove this if error throws
 
-declare_id!("FhnC7vwL3VV6n5kZn85GmRo1k5NeXLLRaHkUYzMh9eXJ");
+// This is your program's public key and it will update
+// automatically when you build the project.
+
+declare_id!("5g4MFuXhq8evZRDgxo1WZLbpX9Ex4MXitYMQ3TCZ2F4U");
 
 pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
 #[program]
-pub mod fav_me {
+pub mod fav_prog {
     use super::*;
 
     pub fn set_favorites(
@@ -15,16 +18,13 @@ pub mod fav_me {
         color: String,
         hobbies: Vec<String>,
     ) -> Result<()> {
-        msg!("Greetings from {}", context.program_id);
         let user_public_key = context.accounts.user.key;
 
-        // Log the user's favorite color and hobbies
-        msg!("User {}'s favorite color is {}", user_public_key, color);
-        msg!(
-            "User {}'s favorite hobbies are: {:?}",
-            user_public_key,
-            hobbies
-        );
+        msg!("Greetings from {}", context.program_id);
+
+        msg!("User {user_public_key}'s favorite number is {number}, favorite color is: {color}",);
+
+        msg!("User's hobbies are: {:?}", hobbies);
 
         context.accounts.favorites.set_inner(Favorites {
             number,
@@ -53,7 +53,7 @@ pub struct SetFavorites<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
-    #[account(init_if_needed, payer = user, space = ANCHOR_DISCRIMINATOR_SIZE, seeds = [b"favorites", user.key().as_ref()], bump)]
+    #[account(init_if_needed, payer = user, space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE, seeds = [b"favorites", user.key().as_ref()], bump)]
     pub favorites: Account<'info, Favorites>,
 
     pub system_program: Program<'info, System>,
